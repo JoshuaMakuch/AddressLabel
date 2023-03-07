a7b8d9f66a1900676e23d0b0ef6768a2b2ae5827
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-03-07 09:00:15 -0700

    Made it so that the user must type in at least something in order to display a label and also included proper options.

--- AddressLabel/AddressLabelForm.vb
+++ AddressLabel/AddressLabelForm.vb
@@ -4,13 +4,24 @@
 'Address Label Program
 'https://github.com/JoshuaMakuch/AddressLabel.git
 
+Option Strict On
+Option Explicit On
+
 Public Class AddressLabelForm
 
     'Whenever the display button is pressed it formats all values from the textboxes and outputs a label
     Private Sub DisplayButton_Click(sender As Object, e As EventArgs) Handles DisplayButton.Click
-        DisplayLabel.Text = $"{FirstNameTextBox.Text} {LastNameTextBox.Text}{vbCrLf}"
-        DisplayLabel.Text = $"{DisplayLabel.Text}{StreetAddressTextBox.Text}{vbCrLf}"
-        DisplayLabel.Text = $"{DisplayLabel.Text}{CityTextBox.Text}, {StateTextBox.Text} {ZipTextBox.Text}"
+
+        'Checks to make sure the user has at least put in something
+        Dim textBoxes As String() = {FirstNameTextBox.Text, LastNameTextBox.Text, StreetAddressTextBox.Text, CityTextBox.Text, StateTextBox.Text, ZipTextBox.Text}
+        If String.Join("", textBoxes).Length > 0 Then
+            DisplayLabel.Text = $"{FirstNameTextBox.Text} {LastNameTextBox.Text}{vbCrLf}"
+            DisplayLabel.Text = $"{DisplayLabel.Text}{StreetAddressTextBox.Text}{vbCrLf}"
+            DisplayLabel.Text = $"{DisplayLabel.Text}{CityTextBox.Text}, {StateTextBox.Text} {ZipTextBox.Text}"
+        Else
+            DisplayLabel.Text = $"Sorry, you must fill out {vbCrLf} at least one field."
+        End If
+
     End Sub
 
     'Sets the textbox to blank
@@ -25,9 +36,6 @@ Public Class AddressLabelForm
 
     'Creats A Tool Tip sub that provides tool tips for each label and button
     Private Sub AddressLabelForm_Load(sender As Object, e As System.EventArgs) Handles MyBase.Load
-        'Disables the display button until the user types in at least something
-        DisplayButton.Hide()
-
         ' Create the ToolTip and associate with the Form container.
         Dim toolTip1 As New ToolTip()
 
@@ -48,9 +56,6 @@ Public Class AddressLabelForm
         toolTip1.SetToolTip(Me.ExitButton, "Exits The Form")
     End Sub
 
-    'Shows the display button so long as the user has typed in something into the first name box
-    Private Sub FirstNameTextBox_TextChanged(sender As Object, e As EventArgs) Handles FirstNameTextBox.TextChanged
-        DisplayButton.Show()
-    End Sub
+
 
 End Class
