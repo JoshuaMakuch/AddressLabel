6c6bbd52e5a778a60331559d9091c3921caf88e2
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-03-07 08:25:18 -0700

    hides the display button until the user types something into the first name box

--- AddressLabel/AddressLabelForm.vb
+++ AddressLabel/AddressLabelForm.vb
@@ -6,8 +6,6 @@
 
 Public Class AddressLabelForm
 
-
-
     'Whenever the display button is pressed it formats all values from the textboxes and outputs a label
     Private Sub DisplayButton_Click(sender As Object, e As EventArgs) Handles DisplayButton.Click
         DisplayLabel.Text = $"{FirstNameTextBox.Text} {LastNameTextBox.Text}{vbCrLf}"
