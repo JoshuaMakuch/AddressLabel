9d874a2709fa694847d47b89308dfe09601b6c4d
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-03-07 08:24:47 -0700

    Hides the display button until the user at least types in something.

--- AddressLabel/AddressLabelForm.vb
+++ AddressLabel/AddressLabelForm.vb
@@ -6,6 +6,8 @@
 
 Public Class AddressLabelForm
 
+
+
     'Whenever the display button is pressed it formats all values from the textboxes and outputs a label
     Private Sub DisplayButton_Click(sender As Object, e As EventArgs) Handles DisplayButton.Click
         DisplayLabel.Text = $"{FirstNameTextBox.Text} {LastNameTextBox.Text}{vbCrLf}"
@@ -25,6 +27,9 @@ Public Class AddressLabelForm
 
     'Creats A Tool Tip sub that provides tool tips for each label and button
     Private Sub AddressLabelForm_Load(sender As Object, e As System.EventArgs) Handles MyBase.Load
+        'Disables the display button until the user types in at least something
+        DisplayButton.Hide()
+
         ' Create the ToolTip and associate with the Form container.
         Dim toolTip1 As New ToolTip()
 
@@ -43,6 +48,11 @@ Public Class AddressLabelForm
         toolTip1.SetToolTip(Me.DisplayButton, "Formats and Displays Users Address")
         toolTip1.SetToolTip(Me.ClearButton, "Clears The Current Label")
         toolTip1.SetToolTip(Me.ExitButton, "Exits The Form")
+    End Sub
+
+    'Shows the display button so long as the user has typed in something into the first name box
+    Private Sub FirstNameTextBox_TextChanged(sender As Object, e As EventArgs) Handles FirstNameTextBox.TextChanged
+        DisplayButton.Show()
     End Sub
 
 End Class
